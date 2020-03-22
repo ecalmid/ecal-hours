9f606c0cf515ff5af91e0223355148c3b26485ba
PTRRR <pietro.alberti@bluewin.ch>
2020-03-22 14:24:46 +0100

    Set bar style to black translucent

--- src/App.vue
+++ src/App.vue
@@ -44,9 +44,9 @@ export default {
     title: 'Hours',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { name: 'apple-mobile-web-app-capable', content: 'yes' },
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }
     ]
   },
 
